945980b934bc3d8c9c39390ae6852ba337e78ad2
Blessen Eby <124340631+Blianokoji@users.noreply.github.com>
2024-09-18 12:23:55 +0530

    Revert "Branch3"

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,37 +1,11 @@
-import React, { useState } from 'react';
-import { Route, Routes, Link, Navigate } from 'react-router-dom';
-import Login from './Login';
-import Booking from './Booking';
-import './App.css';
-import HomePage from './HomePage';
+import './App.css'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // To track login state
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
     <div className="App">
-      <header className="navbar">
-        <nav>
-          <Link to="/">Home</Link>
-          {!isLoggedIn && <Link to="/login" className="login-link">Login</Link>}
-        </nav>
-      </header>
-
-      <Routes>
-        <Route path="/" element={<HomePage isLoggedIn={isLoggedIn} />} />
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        {isLoggedIn ? (
-          <Route path="/booking" element={<Booking />} />
-        ) : (
-          <Route path="/booking" element={<Navigate to="/login" />} />
-        )}
-      </Routes>
+      <h1>HELLO THIS IS SUPPOSED TO BE A FLIGHT BOOKING APP</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
-);
+  </StrictMode>,
+)
